28f5c1bc0edf4641116e5685cb99a51c15c1276e
himani617 <chauhan.himani617@gmail.com>
2026-09-17 17:35:49 +0530

    Added expense summary and accessibilty feature.

---
+++ ExpenseViewerApp/Views/ExpenseSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  ExpenseSummaryView.swift
+//  ExpenseViewerApp
+//
+//  Created by HIMANI VARU on 17/09/26.
+//
+
+import SwiftUI
+
+struct ExpenseSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExpenseSummaryView()
+}
